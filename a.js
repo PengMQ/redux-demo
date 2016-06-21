9abd0bf65f85517113e3c39b8343115ee41f184b
Zhu Ran <zhuran94@163.com>
2016-06-21 11:05:51 +0800

    [Mengqiu] add comments.

--- a.js
+++ a.js
@@ -3,12 +3,19 @@ window.onload = test;
 
 function test(){
 	const createStore = Redux.createStore; //Redux provide the important function createStore which is to create a sore.
-	const store = createStore(counter); // To create a store, the function createStore need a dispatcher as argument.
+	const store = createStore(counter); // When you create a store, you need to specify a 'reducer' to tell store how to manage state.
 
 	var render = function(){
 		document.body.innerText = store.getState();
 
 	}
+	
+	 /*  The store has 3 important methods:
+     store.getState() -> get the current state
+     store.dispatch({type: 'INCREMENT'}) -> dispatch actions to update state
+     store.subscribe(callback) -> It allow you to register a callback, the callback will be invoked anytime when an action is dispatched.
+
+ */
 	store.subscribe(render); //The store need to subscribe with a function which is what would you like to do when the store changed.
 	render();
 
@@ -17,6 +24,11 @@ function test(){
 			});
 }
 
+/* This counter method is very important in Redux, it is call 'reducer'.
+    It accept to arguments: state and action. It's main work is to update the state value based on action type.
+    Because in redux, state is immutable, so you cannot change state directly, you update state by returning values calculated with state.
+    And there are two conventions: 1, need to specify the initial value of state; 2, for default case of action, return state.
+*/
 function counter(state, action){
 	console.log('HAHA', state);
 	if(state === undefined){
